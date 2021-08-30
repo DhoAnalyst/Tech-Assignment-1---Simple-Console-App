a46fae2bdbbaf411000388352e5258e3f171cea4
Duy Ho <duyho@usf.edu>
2021-08-30 12:08:23 -0400

    Commit change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Tech_Assignment_1___Simple_Console_App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, My name is Duy. Nice to meet you!");
+
+            Console.WriteLine("Hope I will get better at C#!");
         }
     }
 }
